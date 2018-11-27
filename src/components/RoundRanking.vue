5ae8c715cfb3a13d286f285b897175009c36df9d
William Di Luigi <williamdiluigi@gmail.com>
2018-11-27 23:59:01 +0100

    round 2 data was released

--- src/components/RoundRanking.vue
+++ src/components/RoundRanking.vue
@@ -13,7 +13,7 @@
               v-for="i in [1, 2, 3, 4]"
               v-bind:key="i"
               v-bind:class="{ 'active': i == parseInt($route.params.roundId),
-                              'disabled': $route.params.editionId === '10' && i > 1 }">
+                              'disabled': $route.params.editionId === '10' && i > 2 }">
             Round {{ i }}
           </router-link>
 
