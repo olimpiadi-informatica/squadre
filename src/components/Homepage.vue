42c06f4d92cf254ed8f1fdb429bdab744f0cd7a8
IOI Admins <ioi@cms>
2019-10-19 18:12:03 +0200

    Update calendar and URLs

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,27 +3,27 @@
     <div class="row">
       <div class="card bg-light col-lg-5 m-2">
         <div class="card-body">
-          <h5 class="card-title">OIS 2018/19</h5>
+          <h5 class="card-title">OIS 2019/20</h5>
           <h6 class="card-subtitle mb-2 text-muted">Official contest</h6>
           <p class="card-text">
             If you are an officially participating team and you have received an
             <strong>official username and password</strong> to participate in
             the <strong>main contest</strong>, click here:
           </p>
-          <a href="/contest/" class="card-link btn btn-outline-primary">Official contest</a>
+          <a href="https://gara.squadre.olinfo.it" class="card-link btn btn-outline-primary">Official contest</a>
         </div>
       </div>
 
       <div class="card bg-light col-lg-5 m-2">
         <div class="card-body">
-          <h5 class="card-title">IIOT Open Contests 2018/19</h5>
+          <h5 class="card-title">IIOT Open Contests 2019/20</h5>
           <h6 class="card-subtitle mb-2 text-muted">Mirrored, open contest</h6>
           <p class="card-text">
             If you don't have an official username and password and you want to
             <strong>signup for a new account</strong> and participate in the
             <strong>mirrored contest</strong>, click here:
           </p>
-          <a href="/contest2/" class="card-link btn btn-outline-primary">Mirror contest</a>
+          <a href="https://mirror.squadre.olinfo.it" class="card-link btn btn-outline-primary">Mirror contest</a>
         </div>
       </div>
     </div>
@@ -38,28 +38,28 @@
       </tr>
       <tr>
         <td>1st contest</td>
-        <td>26th October 2018</td>
-        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20181026T15&p1=215&ah=3">15.00 - 18.00 (GMT+2)</a></td>
+        <td>11th November 2019</td>
+        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20191111T15&p1=215&ah=3">15.00 - 18.00 (CET)</a></td>
       </tr>
       <tr>
         <td>2nd contest</td>
-        <td>27th November 2018</td>
-        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20181128T15&p1=215&ah=3">15:00 - 18:00 (GMT+1)</a></td>
+        <td>17th December 2019</td>
+        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20191217T15&p1=215&ah=3">15:00 - 18:00 (CET)</a></td>
       </tr>
       <tr>
         <td>3rd contest</td>
-        <td>19th December 2018</td>
-        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20181219T1430&p1=215&ah=3">14:30 - 17:30 (GMT+1)</a></td>
+        <td>15th January 2020</td>
+        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20190115T1430&p1=215&ah=3">14:30 - 17:30 (CET)</a></td>
       </tr>
       <tr>
         <td>4th contest</td>
-        <td>10th January 2019</td>
-        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20190110T1430&p1=215&ah=3">14:30 - 17:30 (GMT+1)</a></td>
+        <td>6th February 2020</td>
+        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20200206T1430&p1=215&ah=3">14:30 - 17:30 (CET)</a></td>
       </tr>
       <tr>
         <td>Final contest</td>
-        <td>8th February 2019</td>
-        <td><a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20190208T13&p1=215&ah=3">13:00 - 16:00 (GMT+1)</a></td>
+        <td>21th February 2020</td>
+        <td> <!-- <a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20190208T13&p1=215&ah=3">13:00 - 16:00 (GMT+1)</a> --> </td>
       </tr>
     </table>
   </div>
