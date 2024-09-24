b1382ce30dc06dec67e93dbf0efaeecb13b350d6
bortoz <bortoz@users.noreply.github.com>
2024-09-24 08:58:19 +0000

    deploy: e8e5edda7209ee2d921da14b9c834076acf2314d

---
+++ _next/static/chunks/7086.82005efa796b4bfd.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunk_N_E=globalThis.webpackChunk_N_E||[]).push([[7086],{37086:(a,s,e)=>{e.r(s),e.d(s,{default:()=>h.ZP,getWasmInstance:()=>h.pC,wasmBinary:()=>h.HS});var h=e(95209)}}]);
