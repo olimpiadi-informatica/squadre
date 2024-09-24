582cc838e1ebe5b08c8be8f27101d433a867bb74
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2024-09-24 10:44:58 +0200

    Improve colors

--- src/components/score.tsx
+++ src/components/score.tsx
@@ -13,11 +13,15 @@ export function Score({
 
   return (
     <span
-      className={clsx("block rounded-box", className, {
+      className={clsx("block rounded-box text-black border border-black/10", className, {
         "font-bold": percent === 100,
-        "bg-success/90 text-success-content": percent > 80,
-        "bg-warning/90 text-warning-content": percent > 40 && percent <= 80,
-        "bg-error/90 text-error-content": percent <= 40,
+        "bg-emerald-600": percent > 90,
+        "bg-emerald-500": percent > 75 && percent <= 90,
+        "bg-emerald-400": percent > 60 && percent <= 75,
+        "bg-emerald-300": percent > 45 && percent <= 60,
+        "bg-emerald-200": percent > 30 && percent <= 45,
+        "bg-emerald-100": percent > 15 && percent <= 30,
+        "bg-emerald-50": percent <= 15,
       })}>
       {score}
     </span>
