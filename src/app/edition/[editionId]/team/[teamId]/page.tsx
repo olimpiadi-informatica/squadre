7da09adc73b7e50912818135a7d14e439ca58407
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2024-12-06 20:42:46 +0100

    Fix overflow

--- src/app/edition/[editionId]/team/[teamId]/page.tsx
+++ src/app/edition/[editionId]/team/[teamId]/page.tsx
@@ -61,8 +61,13 @@ export default async function Page({ params: { editionId, teamId } }: Props) {
         </ul>
       </div>
       <div className="grid gap-4 md:grid-cols-2">
-        <Card>
-          <CardBody title={`${team.name} (${team.edition}, ${team.year})`}>
+        <Card className="*:w-full">
+          <CardBody
+            title={
+              <div className="break-words w-full">
+                {team.name} ({team.edition}, {team.year})
+              </div>
+            }>
             <p>
               <Link href={`/region/${team.region}/${team.inst_id}`} className="link">
                 {team.institute}
@@ -73,7 +78,7 @@ export default async function Page({ params: { editionId, teamId } }: Props) {
               </Link>
             </p>
             <p className="font-bold text-base-content/60">Coach: {team.coach}</p>
-            <p>
+            <p className="break-words">
               {team.name} scored {team.points} points, ranking {team.rank_tot} in Italy and{" "}
               {team.rank_reg} in {team.fullregion}; for an average ranking of{" "}
               {Math.round(team.avgrank)}%, and an highest rank achieved in a contest of{" "}
