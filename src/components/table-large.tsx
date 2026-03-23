e6a4f8099508877d0b57e7739fc3ab90d8e678b1
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2026-03-23 21:02:50 +0100

    Improve table

--- src/components/table-large.tsx
+++ src/components/table-large.tsx
@@ -1,4 +1,12 @@
-import { Fragment, forwardRef, useLayoutEffect, useRef, useState } from "react";
+import {
+  Fragment,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 
 import clsx from "clsx";
 import { type Components, Virtuoso } from "react-virtuoso";
@@ -14,6 +22,7 @@ export default function LargeTable<T>({
 }: TableProps<T>) {
   const ref = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState(0);
+  const [gridTemplateColumns, setGridTemplateColumns] = useState<string | undefined>();
 
   useLayoutEffect(() => {
     const obs = new ResizeObserver((entries) => {
@@ -26,6 +35,36 @@ export default function LargeTable<T>({
     return () => obs.disconnect();
   }, []);
 
+  const updateColumnWidths = useCallback(() => {
+    if (!ref.current) return;
+    const viewport = ref.current.children[0].children[0];
+    const headers = viewport.children[0];
+    const headerWidths = Array.from(
+      headers.children,
+      (header, i) => header.clientWidth + (i === 0 || i === headers.children.length - 1 ? 8 : 0),
+    );
+    setGridTemplateColumns(headerWidths.map((header) => `${header}px`).join(" "));
+  }, []);
+
+  useEffect(() => {
+    updateColumnWidths();
+
+    const abortController = new AbortController();
+    let id: ReturnType<typeof setTimeout> | undefined;
+
+    window.addEventListener(
+      "resize",
+      () => {
+        clearTimeout(id);
+        setGridTemplateColumns(undefined);
+        id = setTimeout(() => updateColumnWidths(), 20);
+      },
+      { signal: abortController.signal },
+    );
+
+    return () => abortController.abort();
+  }, [updateColumnWidths]);
+
   return (
     <div className={style.outerContainer}>
       <div className={style.innerContainer} ref={ref} style={{ width }}>
@@ -34,7 +73,9 @@ export default function LargeTable<T>({
           itemContent={(_index, item) => <Row item={item} />}
           components={{ Header, Footer, List, Item }}
           increaseViewportBy={200}
+          initialItemCount={10}
           className={clsx(style.scroller, className)}
+          style={{ gridTemplateColumns }}
           useWindowScroll
         />
       </div>
